cc15064edd92950bd6a9a8d86427af54f99ce791
Tommy Tran <tranttommy95@gmail.com>
2019-02-25 16:22:19 -0800

    test passed with interpolations

--- test/make-card-template.test.js
+++ test/make-card-template.test.js
@@ -2,17 +2,22 @@ const test = QUnit.test;
 
 QUnit.module('Making Photocard Template');
 
-function makeCardTemplate() {
+function makeCardTemplate(image) {
     return `
     <li>
-        <h2>Basically a unicorn</h2>
-        <img src="https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg">
+        <h2>${image.title}</h2>
+        <img src="${image.url}">
     </li>
     `;
 }
 
 test('Input image object and have function return card template', assert => {
     // Arrange
+    const image = {
+        title: 'Basically a unicorn',
+        url: 'https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg'
+    };
+
     const expected = /*html*/ `
     <li>
         <h2>Basically a unicorn</h2>
@@ -21,7 +26,7 @@ test('Input image object and have function return card template', assert => {
     `;
     
     // Act
-    const result = makeCardTemplate();
+    const result = makeCardTemplate(image);
 
     // Assert
     assert.equal(result, expected);
